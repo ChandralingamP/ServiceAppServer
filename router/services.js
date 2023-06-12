524541ba45baa7c8a9658f13e478980ddd6eb22d
ChandralingamP <chandralingam2003@gmail.com>
2023-06-12 15:35:19 +0530

    service

--- router/services.js
+++ router/services.js
@@ -1,15 +1,31 @@
 const router = require("express").Router();
 const db = require("../db/database");
 
-router.route('/details').get(async(req,res)=>{
-    let getServiceQuery = `SELECT * FROM servicestypes WHERE 1`
-    db.execute(getServiceQuery,(err,result)=>{
-        if(err){
-            res.status(400).json(err);
-        }else{
-            res.status(200).json(result);
-        }
-    })
-})
+router.route("/details").get(async (req, res) => {
+  console.log("hi");
+  let getServiceQuery = `SELECT * FROM services ORDER BY serviceType`;
+
+  db.execute(getServiceQuery, (err, result) => {
+    if (err) {
+      res.status(400).json(err);
+    } else {
+      const AcServices = result.filter((item) => item.serviceType == "AC");
+      const ElectricalServices = result.filter(
+        (item) => item.serviceType == "Electrical"
+      );
+      const PlumbingServices = result.filter(
+        (item) => item.serviceType == "Plumbing"
+      );
+      console.log("hi");
+      res
+        .status(200)
+        .json({
+          AcServices: AcServices,
+          ElectricalServices: ElectricalServices,
+          PlumbingServices: PlumbingServices,
+        });
+    }
+  });
+});
 
 module.exports = router;
